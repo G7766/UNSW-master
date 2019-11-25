8c4401bb070a2dc3ccc6c1ba45cb58045ba66276
G <gk2033724664@gmail.com>
2019-11-26 01:43:18 +1100

    delete

--- 9900 Software as a Service Project/project/9900/capstone-project-betago/BetaGo/server/weebhook_server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask import request,make_response,jsonify
-app = Flask(__name__)
-
-# web hook server
-@app.route('/webhook',methods= ['POST','GET'])
-def webhook():
-    req = request.get_json(silent=True)
-    return jsonify(req)
